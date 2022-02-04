48843960145ebe5967bda29c9ac85c616191a633
bharanisaisudheer1234 <bharanisaisudheer1234@gmail.com>
2022-02-04 18:20:43 +0530

    method overloading

---
+++ 10 th day morning assignments/10th day project3/10th day project3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10th_day_project3
+{
+    class sai
+    {
+        public int add (int a, int b)
+        {
+            return a + b;
+        }
+        public int add (int a, int b, int c)
+        {
+            return a + b + c;   
+        }
+        public int add (int a, int b, int c, int d)
+        {
+            return a+b + c + d; 
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sai pn = new sai();
+            Console.WriteLine(pn.add(5,6));
+            Console.ReadLine();
+        }
+    }
+}
