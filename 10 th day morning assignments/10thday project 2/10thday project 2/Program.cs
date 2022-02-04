c2ff301370551e567847be4489991cd6ad5e55d8
bharanisaisudheer1234 <bharanisaisudheer1234@gmail.com>
2022-02-04 18:19:44 +0530

    multilevel inheritance

---
+++ 10 th day morning assignments/10thday project 2/10thday project 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using _10th_day_project2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10th_day_project2
+{
+    class sudheer
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    class totalmaths : sudheer
+    {
+        public int mul(int a, int b)
+        {
+            return (a * b);
+        }
+
+
+    }
+    class saisubjects : totalmaths
+    {
+        public string sai()
+        {
+            return "yuvi";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            saisubjects tm = new saisubjects();
+            Console.WriteLine(tm.add(5,6));
+            Console.WriteLine(tm.sai());
+            
+                Console.ReadLine();
+        }
+    }
+}
+
