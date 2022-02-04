eabb399492a65288f60c3a8eb2bb664b3cc9d9d9
bharanisaisudheer1234 <bharanisaisudheer1234@gmail.com>
2022-02-04 18:22:55 +0530

    method overriding

---
+++ 10 th day morning assignments/10th day project 4/10th day project 4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _10th_day_project_4
+{
+    class english 
+    {
+                   public void printsai()
+                   {
+                       Console.WriteLine("sai");
+                   }
+
+                   public void printsudheer()
+                   {
+                          Console.WriteLine("sudheer");
+
+                   }
+                   public  void printpavan()
+                   {
+                         Console.WriteLine("pavan");
+
+                   }
+
+     }
+     class telug : english
+     {
+        public new void printpavan()
+       {
+        Console.WriteLine("yuvi");
+
+        }
+
+     }
+      
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            telug t1 = new telug();
+            t1.printsudheer();
+            
+
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
