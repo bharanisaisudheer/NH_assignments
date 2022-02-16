df1e760463df01695db89a3ea8daa26af7c2ab30
bharanisaisudheer <bharanisaisudheer1234@gmail.com>
2022-02-17 00:18:06 +0530

    day18

---
+++ day18/18dayproject1/18dayproject1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mathsLibrary;
+
+namespace _18dayproject1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Algebra.Factorial(n));
+            Console.ReadLine();
+        }
+    }
+}
